78b54562e3f56e1751e6134b2f83f15d5b9979e7
Max Bachhuber <max.bachhuber@bahuma.io>
2023-05-23 17:26:32 +0200

    Update main.js
    Remove unused import

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //
 // This includes Popper and all of Bootstrap's JS plugins.
 
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 //
