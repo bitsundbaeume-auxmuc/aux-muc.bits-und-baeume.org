eefd14c69bc6981e6d1f646a9308f13e35ae0ab4
Max Bachhuber <max.bachhuber@bahuma.io>
2023-06-27 00:08:07 +0200

    Check permissions on cron urls

--- src/Controller/CronController.php
+++ src/Controller/CronController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Service\NotifyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CronController extends AbstractController
@@ -18,8 +21,19 @@ class CronController extends AbstractController
     }
 
     #[Route('/cron/notify-events')]
-    public function notifyEvents()
+    public function notifyEvents(Request $request)
     {
+        $this->checkKey($request);
         $this->notifyService->notify();
+        return new Response('OK');
+    }
+
+    protected function checkKey(Request $request) {
+        $key = $request->get('key');
+        $cronKey = $_ENV['CRON_KEY'];
+
+        if ($key !== $cronKey) {
+            throw new HttpException(403, "Request parameter 'key' is missing or wrong.");
+        }
     }
 }
